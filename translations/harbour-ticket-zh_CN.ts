66f9709fa388739090d843f190afa2a8b643f3a7
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-01-05 02:10:47 +0800

    Update harbour-ticket-zh_CN.ts

--- translations/harbour-ticket-zh_CN.ts
+++ translations/harbour-ticket-zh_CN.ts
@@ -1,762 +1,433 @@
 <?xml version="1.0" encoding="utf-8"?>
-
 <!DOCTYPE TS>
-
 <TS version="2.1">
-
 <context>
-
     <name>AirlinesOffers</name>
-
     <message>
-
         <source>Special offers</source>
-
         <translation>特价票</translation>
-
     </message>
-
 </context>
-
 <context>
-
     <name>ComplexSearchDialog</name>
-
     <message>
-
         <source>Select</source>
-
         <translation>选择</translation>
-
     </message>
-
     <message>
-
         <source>Econom class</source>
-
         <translation>经济舱</translation>
-
     </message>
-
     <message>
-
         <source>Busines class</source>
-
         <translation>商务舱</translation>
-
     </message>
-
     <message>
-
         <source>Origin:</source>
-
-        <translation起始地:></translation>
-
+        <translation>出发地：</translation>
     </message>
-
     <message>
-
         <source>Destination:</source>
-
-        <translation>目的地:</translation>
-
+        <translation>目的地：</translation>
     </message>
-
     <message>
-
         <source>Departure date:</source>
-
-        <translation>出发日期:</translation>
-
+        <translation>出发日期：</translation>
     </message>
-
     <message>
-
         <source>Search</source>
-
         <translation>搜索</translation>
-
     </message>
-
     <message>
-
         <source>Cancel</source>
-
         <translation>取消</translation>
-
     </message>
-
     <message>
-
         <source>Adults count</source>
-
-        <translation>成人票</translation>
-
+        <translation>成人数量</translation>
     </message>
-
     <message>
-
-        <source>Childrens count</source>
-
-        <translation>儿童票</translation>
-
-    </message>
-
-    <message>
-
         <source>Trip class:</source>
-
-        <translation>座位等级:</translation>
-
+        <translation>机舱等级：</translation>
     </message>
-
+    <message>
+        <source>Children count</source>
+        <translation>儿童数量</translation>
+    </message>
 </context>
-
 <context>
-
     <name>FavoritesDelegate</name>
-
     <message>
-
         <source>Departure: </source>
-
-        <translation>离站:</translation>
-
+        <translation>出发：</translation>
     </message>
-
 </context>
-
 <context>
-
     <name>SearchAirportDialog</name>
-
     <message>
-
         <source>Search airports:</source>
-
-        <translation>搜索机场:</translation>
-
+        <translation>搜索机场：</translation>
     </message>
-
     <message>
-
         <source>No airports found</source>
-
         <translation>没有找到机场</translation>
-
     </message>
-
     <message>
-
         <source>Enter airport/city name</source>
-
-        <translation>输入机场或城市名称</translation>
-
+        <translation>输入机场/城市名称</translation>
     </message>
-
     <message>
-
         <source>Location: </source>
-
-        <translation>位置:</translation>
-
+        <translation>位置：</translation>
     </message>
-
 </context>
-
 <context>
-
     <name>SearchDialog</name>
-
     <message>
-
         <source>Search</source>
-
         <translation>搜索</translation>
-
     </message>
-
     <message>
-
         <source>Cancel</source>
-
         <translation>取消</translation>
-
     </message>
-
     <message>
-
         <source>Origin:</source>
-
-        <translation>出发点:</translation>
-
+        <translation>出发地：</translation>
     </message>
-
     <message>
-
         <source>Destination:</source>
-
-        <translation>距离:</translation>
-
+        <translation>目的地：</translation>
     </message>
-
     <message>
-
         <source>Departure date:</source>
-
-        <translation>起飞时间:</translation>
-
+        <translation>出发日期：</translation>
     </message>
-
     <message>
-
         <source>Return date:</source>
-
-        <translation>返程日期:</translation>
-
+        <translation>回程日期：</translation>
     </message>
-
     <message>
-
         <source>Adults count</source>
-
-        <translation>成人票</translation>
-
+        <translation>成人数量</translation>
     </message>
-
     <message>
-
         <source>Without transfer</source>
-
         <translation>不转机</translation>
-
     </message>
-
     <message>
-
         <source>Seat class:</source>
-
-        <translation>座位等级</translation>
-
+        <translation>座位等级：</translation>
     </message>
-
     <message>
-
         <source>Econom class</source>
-
         <translation>经济舱</translation>
-
     </message>
-
     <message>
-
         <source>Busines class</source>
-
-        <translation>商务车</translation>
-
+        <translation>商务舱</translation>
     </message>
-
     <message>
-
         <source>Select</source>
-
         <translation>选择</translation>
-
     </message>
-
     <message>
-
         <source>Childrens count</source>
-
-        <translation>儿童票</translation>
-
+        <translation>儿童数量</translation>
     </message>
-
 </context>
-
 <context>
-
     <name>SearchResultsPage</name>
-
     <message>
-
         <source>Results</source>
-
         <translation>结果</translation>
-
     </message>
-
     <message>
-
         <source>Found: </source>
-
-        <translation>找到:</translation>
-
+        <translation>找到：</translation>
     </message>
-
     <message>
-
         <source>Sort by price: </source>
-
-        <translation>按票价排序:</translation>
-
+        <translation>按票价排序：</translation>
     </message>
-
     <message>
-
         <source>low to high</source>
-
         <translation>从低到高</translation>
-
     </message>
-
     <message>
-
         <source>Search is in progress</source>
-
         <translation>正在搜索</translation>
-
     </message>
-
     <message>
-
         <source>No tickets found</source>
-
         <translation>没有找到机票</translation>
-
     </message>
-
     <message>
-
         <source>Please wait</source>
-
         <translation>请稍候</translation>
-
     </message>
-
     <message>
-
         <source>Please, change the search request</source>
-
-        <translation><请更改搜索请求/translation>
-
+        <translation>请更改搜索请求</translation>
     </message>
-
     <message>
-
         <source>Sort by duration</source>
-
-        <translation>按飞行时间分类</translation>
-
+        <translation>按时长排序</translation>
     </message>
-
 </context>
-
 <context>
-
     <name>SettingsPage</name>
-
     <message>
-
         <source>Language</source>
-
         <translation>语言</translation>
-
     </message>
-
     <message>
-
         <source>EN</source>
-
         <translation>英语</translation>
-
     </message>
-
     <message>
-
         <source>RU</source>
-
         <translation>俄语</translation>
-
     </message>
-
     <message>
-
         <source>DE</source>
-
         <translation>德语</translation>
-
     </message>
-
     <message>
-
         <source>Currency</source>
-
         <translation>货币</translation>
-
     </message>
-
     <message>
-
         <source>USD</source>
-
         <translation>美元</translation>
-
     </message>
-
     <message>
-
         <source>EUR</source>
-
         <translation>欧元</translation>
-
     </message>
-
     <message>
-
         <source>RUB</source>
-
         <translation>卢布</translation>
-
     </message>
-
     <message>
-
         <source>Search settings</source>
-
         <translation>搜索设置</translation>
-
     </message>
-
     <message>
-
         <source>Application settings</source>
-
         <translation>软件设置</translation>
-
     </message>
-
     <message>
-
         <source>Show hints</source>
-
-        <translation>显示提醒</translation>
-
+        <translation>显示提示</translation>
     </message>
-
     <message>
-
         <source>Convert price to</source>
-
         <translation>转换票价为</translation>
-
     </message>
-
     <message>
-
         <source>FR</source>
-
         <translation>法语</translation>
-
     </message>
-
     <message>
-
         <source>IT</source>
-
         <translation>意大利语</translation>
-
     </message>
-
     <message>
-
         <source>PL</source>
-
         <translation>波兰语</translation>
-
     </message>
-
     <message>
-
         <source>TH</source>
-
         <translation>泰语</translation>
-
     </message>
-
     <message>
-
         <source>Delete history</source>
-
         <translation>删除历史</translation>
-
     </message>
-
     <message>
-
         <source>Settings</source>
-
         <translation>设置</translation>
-
     </message>
-
     <message>
-
         <source>Open links in browser</source>
-
-        <translation>在浏览器打开</translation>
-
+        <translation>用浏览器打开链接</translation>
     </message>
-
+    <message>
+        <source>SEK</source>
+        <translation>瑞典克朗</translation>
+    </message>
+    <message>
+        <source>CNY</source>
+        <translation>人民币</translation>
+    </message>
+    <message>
+        <source>GBP</source>
+        <translation>英镑</translation>
+    </message>
+    <message>
+        <source>CZK</source>
+        <translation>捷克克朗</translation>
+    </message>
+    <message>
+        <source>PLN</source>
+        <translation>波兰兹罗提</translation>
+    </message>
+    <message>
+        <source>JPY</source>
+        <translation>日元</translation>
+    </message>
 </context>
-
 <context>
-
     <name>StartPage</name>
-
     <message>
-
         <source>Avia tickets</source>
-
         <translation>Avia tickets</translation>
-
     </message>
-
     <message>
-
         <source>One way ticket</source>
-
-        <translation>一步查询机票</translation>
-
+        <translation>单程票</translation>
     </message>
-
     <message>
-
         <source>About</source>
-
-        <translation><关于/translation>
-
+        <translation>关于</translation>
     </message>
-
     <message>
-
         <source>Settings</source>
-
         <translation>设置</translation>
-
     </message>
-
     <message>
-
         <source>Pull down and select Search</source>
-
         <translation>下拉以搜索</translation>
-
     </message>
-
     <message>
-
         <source>Delete</source>
-
         <translation>删除</translation>
-
     </message>
-
     <message>
-
         <source>Search history</source>
-
         <translation>搜索历史</translation>
-
     </message>
-
     <message>
-
         <source>One way search</source>
-
-        <translation>一步搜索</translation>
-
+        <translation>单程票搜索</translation>
     </message>
-
     <message>
-
         <source>Round trip search</source>
-
-        <translation>搜索往返机票</translation>
-
+        <translation>来回票</translation>
     </message>
-
     <message>
-
         <source>Round trip</source>
-
         <translation>往返旅行</translation>
-
     </message>
-
     <message>
-
         <source>Complex search</source>
-
-        <translation>高级搜索</translation>
-
+        <translation></translation>
     </message>
-
     <message>
-
         <source>Fly with more than two stops</source>
-
-        <translation>有两次中途经停</translation>
-
+        <translation>飞行有超过两次经停</translation>
     </message>
-
     <message>
-
         <source>Search history is empty</source>
-
         <translation>搜索历史为空</translation>
-
     </message>
-
     <message>
-
         <source>Compensation for flight delay or cancellation</source>
-
-        <translation>航班延误及取消赔偿</translation>
-
+        <translation>航班晚点及取消补偿</translation>
     </message>
-
     <message>
-
         <source>Rent a car</source>
-
         <translation>租车</translation>
-
     </message>
-
 </context>
-
 <context>
-
-    <name>TicketInfoDelegate</name>
-
+    <name>TicketDelegate</name>
     <message>
-
-        <source>Direct</source>
-
-        <translation>直飞</translation>
-
-    </message>
-
-    <message>
-
-        <source>&lt;b&gt;Trip duration:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;飞行时间:&lt;/b&gt;</translation>
-
-    </message>
-
-    <message>
-
-        <source>&lt;b&gt;Stops:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;经停:&lt;/b&gt;</translation>
-
-    </message>
-
-</context>
-
-<context>
-
-    <name>TicketPage</name>
-
-    <message>
-
-        <source>Buy</source>
-
-        <translation>购买</translation>
-
-    </message>
-
-    <message>
-
         <source>&lt;b&gt;Flight number:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;航班编号:&lt;/b&gt;</translation>
-
+        <translation>&lt;b&gt;航班数量:&lt;/b&gt; <</translation>
     </message>
-
     <message>
-
         <source>&lt;br&gt;&lt;b&gt;Aircraft:&lt;/b&gt; </source>
-
-        <translation>&lt;br&gt;&lt;b&飞机:&lt;/b&gt;</translation>
-
+        <translation>&lt;br&gt;&lt;b&gt;飞机:&lt;/b&gt; </translation>
     </message>
-
     <message>
-
-        <source>&lt;b&gt;Origin:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;起点:&lt;/b&gt;</translation>
-
+        <source>&lt;b&gt;Trip duration:&lt;/b&gt;&lt;br&gt;</source>
+        <translation>&lt;b&gt;旅程时长:&lt;/b&gt;&lt;br&gt;</translation>
     </message>
-
     <message>
-
-        <source>&lt;b&gt;Depature:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;起飞:&lt;/b&gt;</translation>
-
+        <source>&lt;b&gt;Time to wait:&lt;/b&gt; </source>
+        <translation>&lt;b&gt;等候时间:&lt;/b&gt;</translation>
     </message>
-
     <message>
-
-        <source>&lt;b&gt;Destination:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;目的地:&lt;/b&gt; </translation>
-
+        <source>Baggage: </source>
+        <translation>行李：</translation>
     </message>
-
     <message>
-
-        <source>&lt;b&gt;Arrival:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;抵达:&lt;/b&gt; </translation>
-
+        <source>Handbags: </source>
+        <translation>旅行箱：</translation>
     </message>
-
-    <message>
-
-        <source>&lt;b&gt;Trip duration:&lt;/b&gt; </source>
-
-        <translation>&lt;b&gt;飞行时间:&lt;/b&gt;</translation>
-
-    </message>
-
 </context>
-
 <context>
-
-    <name>Utils</name>
-
+    <name>TicketInfoDelegate</name>
     <message>
-
-        <source> h </source>
-
-        <translation>小时</translation>
-
+        <source>Direct</source>
+        <translation>直飞</translation>
     </message>
-
     <message>
-
-        <source> m</source>
-
-        <translation>分钟</translation>
-
+        <source>&lt;b&gt;Trip duration:&lt;/b&gt; </source>
+        <translation>>&lt;b&gt;旅程时长:&lt;/b&gt; </translation>
     </message>
-
+    <message>
+        <source>&lt;b&gt;Stops:&lt;/b&gt; </source>
+        <translation>&lt;b&gt;经停:&lt;/b&gt; </translation>
+    </message>
 </context>
-
+<context>
+    <name>TicketPage</name>
+    <message>
+        <source>Buy</source>
+        <translation>购票</translation>
+    </message>
+</context>
+<context>
+    <name>Utils</name>
+    <message>
+        <source> h </source>
+        <translation>小时</translation>
+    </message>
+    <message>
+        <source> m</source>
+        <translation>分钟</translation>
+    </message>
+    <message>
+        <source>luggage is not included in price</source>
+        <translation>行李未包含在票价内</translation>
+    </message>
+    <message>
+        <source>No information about luggage</source>
+        <translation>没有关于行李对我信息</translation>
+    </message>
+    <message>
+        <source>no luggage</source>
+        <translation>不含行李</translation>
+    </message>
+    <message>
+        <source> kg</source>
+        <translation>千克</translation>
+    </message>
+    <message>
+        <source> luggage</source>
+        <translation>行李</translation>
+    </message>
+    <message>
+        <source>luggage of </source>
+        <translation>行李</translation>
+    </message>
+    <message>
+        <source> luggage(s)
+ of </source>
+        <translation>行李</translation>
+    </message>
+</context>
 </TS>
-
